0c7f535718e00969bc90fc9154ffa8bc98e60490
Nikita Mittal <nikitamittal2423@gmail.com>
2026-03-24 16:47:14 +0530

    changing catalogue endpoint

--- lib/utils.ts
+++ lib/utils.ts
@@ -3,7 +3,7 @@ import { twMerge } from "tailwind-merge";
 
 // const BACKEND_IP = "13.250.125.230";
 const BACKEND_IP = "35.240.222.126"; // new google server
-// const BACKEND_IP = "localhost";
+// const BACKEND_IP = "localhost:3000";
 
 export const NEXT_PUBLIC_API_URL = `http://${BACKEND_IP}`;
 export const PYTHON_BACKEND_URL = `http://${BACKEND_IP}:8000`;
